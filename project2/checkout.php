c6e0f8c240c576c5da086355eb8e19389286251b
Ken131313 <lokhimwong111@gmail.com>
2023-11-22 18:30:15 +0000

    updated checkout page

--- project2/checkout.php
+++ project2/checkout.php
@@ -1,10 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Assuming you have a database connection established
+    // Replace with your actual database credentials
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "project2";
+    $dbname = "test";
+
+    // Create a database connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
     // Check connection
@@ -12,28 +14,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . mysqli_connect_error());
     }
 
+    // Retrieve payment details from the form
+    $stripeToken = $_POST['stripeToken'];
+
     // Retrieve user details from the form
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $address = $_POST['address'];
 
-    // Insert user details into the database
-    $userInsertQuery = "INSERT INTO users (first_name, last_name, address) VALUES ('$firstName', '$lastName', '$address')";
-    if (mysqli_query($conn, $userInsertQuery)) {
-        echo "User details inserted successfully";
-    } else {
-        echo "Error inserting user details: " . mysqli_error($conn);
-    }
-
-    // Retrieve dummy payment token (you would handle this differently in a real scenario)
-    $token = $_POST['token'];
-
     // Insert payment information into the database
-    $paymentInsertQuery = "INSERT INTO payments (user_id, token) VALUES (LAST_INSERT_ID(), '$token')";
+    $paymentInsertQuery = "INSERT INTO payment (stripe_token) VALUES ('$stripeToken')";
     if (mysqli_query($conn, $paymentInsertQuery)) {
-        echo "Payment details inserted successfully";
+        echo "Payment details inserted successfully. ";
     } else {
         echo "Error inserting payment details: " . mysqli_error($conn);
+    }
+
+    // Insert user information into the database
+    $userInsertQuery = "INSERT INTO user (first_name, last_name, address) VALUES ('$firstName', '$lastName', '$address')";
+    if (mysqli_query($conn, $userInsertQuery)) {
+        echo "User details inserted successfully.";
+    } else {
+        echo "Error inserting user details: " . mysqli_error($conn);
     }
 
     // Close database connection
